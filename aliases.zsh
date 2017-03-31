082cbf6833744fc0f86b114e651f5897d0b1878d
Rob Phoenix <rob@robphoenix.com>
2017-03-31 17:35:00 +0100

    add fod

--- aliases.zsh
+++ aliases.zsh
@@ -23,7 +23,7 @@ alias gpg="gpg2"
 alias u="usb"
 alias uu="sudo umount /media/usb"
 # start vpn
-alias vpnu="sudo openvpn /etc/openvpn/uk-01.protonvpn.conf &"
+alias vpnu="sudo openvpn /etc/openvpn/uk-proton.conf &"
 alias vpnd="sudo killall openvpn"
 # zsh
 alias relo="source ~/.zshrc"
