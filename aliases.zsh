057d92eb6b5780a66c60b0827fcab970232e81f6
Rob Phoenix <rob@robphoenix.com>
2018-10-08 21:19:58 +0100

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,3 @@
-# Personal aliases
-
-# system
 alias ls=ls
 alias l="LC_COLLATE=C ls -lAhF -I.dropbox -I.dropbox.cache -I.cache -I__pycache__ -I.git -Inode_modules -I.vagrant -I.DS_Store"
 alias mv="mv -ivu"
@@ -9,21 +6,13 @@ alias say="sudo apt -y"
 alias sarepo="sudo add-apt-repository"
 alias sudo="sudo "
 alias su="sudo -i"
-alias up="sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove"
+alias apt-up="sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove"
 alias rmd="sudo rm -r"
 alias cpd="sudo cp -r"
-alias off="sudo shutdown now"
-alias restart="sudo reboot now"
-alias open="xdg-open"
-alias gpg="gpg2"
-alias music="cmus"
+alias p1="ping 1.1.1.1"
+alias p8="ping 8.8.8.8"
 # zsh
 alias relo="source ~/.zshrc"
-# tmux
-alias tls="tmux ls"
-alias tn="tmux new -s"
-alias ta="tmux attach -t"
-alias tk="tmux kill-session -t"
 # go
 alias gg="go get -u -v"
 alias gt="gotest"
@@ -31,9 +20,6 @@ alias gtv="gotest -v -cover"
 alias gox="gox -output=\"bin/{{.Dir}}_{{.OS}}_{{.Arch}}\""
 # git
 alias g="git"
-# exercism
-alias exf="exercism fetch"
-alias exs="exercism submit"
 # z
 alias j=z
 alias jj=zz
