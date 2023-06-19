6eabcf0baf63ea04559be96b74b2d6d1f178a27d
Rob Phoenix <rphoenix@uw.co.uk>
2023-06-19 14:10:54 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -3,52 +3,52 @@
 # reference: https://github.com/jalexanderfox/dotfiles/blob/master/setup_brew.sh
 
 # Ask for the administrator password upfront.
-sudo -v
+# sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install Brew
 # Check for Homebrew,
 # Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+# if test ! $(which brew); then
+  # echo "Installing homebrew..."
+  # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
 
 # Update homebrew recipes
-brew update
+# brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade
+# brew upgrade
 
 # List of binaries to install
 # sshfs
-binaries=(
-  bat
-  gh
-  neovim
-  node
-  python
-  ripgrep
-  trash
-  tree
-  z
-  zsh-completions
-  zsh-syntax-highlighting
-)
-
-echo "installing binaries..."
-brew install ${binaries[@]}
-
-brew tap homebrew/cask-fonts #You only need to do this once for cask-fonts
-brew install --cask font-fantasque-sans-mono
+# binaries=(
+#   bat
+#   gh
+#   neovim
+#   node
+#   python
+#   ripgrep
+#   trash
+#   tree
+#   z
+#   zsh-completions
+#   zsh-syntax-highlighting
+# )
+#
+# echo "installing binaries..."
+# brew install ${binaries[@]}
+#
+# brew tap homebrew/cask-fonts #You only need to do this once for cask-fonts
+# brew install --cask font-fantasque-sans-mono
 
 # Cleanup
-brew cleanup
+# brew cleanup
 
 # Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Symlinks
 ln -sf "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
@@ -62,11 +62,11 @@ ln -sf "$HOME/dotfiles/functions.zsh" "$HOME/.oh-my-zsh/custom/functions.zsh"
 ln -sf "$HOME/dotfiles/secrets.zsh" "$HOME/.oh-my-zsh/custom/secrets.zsh"
 ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-npm install -g corepack
-pip3 install --upgrade pynvim
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# npm install -g corepack
+# pip3 install --upgrade pynvim
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
